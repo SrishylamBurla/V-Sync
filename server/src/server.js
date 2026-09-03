2ebabbb565e8a1d1c158c339c7b8d0e729b22a76
Srishylam Burla <srishylamburla1@gmail.com>
2026-09-03 14:44:38 +0530

    build

--- server/src/server.js
+++ server/src/server.js
@@ -32,13 +32,31 @@ const app = express();
 
 app.use(helmet());
 
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
+
+const allowedOrigins = [
+  "https://v-sync.in",
+  "https://www.v-sync.in",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
-
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
